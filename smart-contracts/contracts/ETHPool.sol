ed7b71bd762feade78035c3ce1c2ecb805b6d59b
C-Mierez <carlos.mierez20@gmail.com>
2022-03-11 17:37:41 -0300

    WIP: Added Withdraw

--- smart-contracts/contracts/ETHPool.sol
+++ smart-contracts/contracts/ETHPool.sol
@@ -10,14 +10,29 @@ contract ETHPool {
     */
 
     /* --------------------------------- Staking -------------------------------- */
-    // mapping(address => bool) isUserStaking; // Maybe?
-    mapping(address => uint256) userStakedAmount;
+    mapping(address => uint256) public userStakedAmount;
 
     /* --------------------------------- Staking -------------------------------- */
     function stakeETH() public payable {
         require(msg.value > 0, "Must send a positive amount of ETH.");    
 
         userStakedAmount[msg.sender] += msg.value;
+    }
+
+    function withdrawETH() public {
+        uint256 staked = userStakedAmount[msg.sender];
+        require(staked > 0, "Must have staked ETH to withdraw.");
+        
+        userStakedAmount[msg.sender] = 0;
+
+        // AFAIK This is the preferred way of transferring ETH currently
+        (bool sent, ) = msg.sender.call{value: staked}("");
+        require(sent, "Failed to send Ether.");
+    }
+
+    /* ---------------------------------- View --------------------------------- */
+    function getPoolBalance() public view returns (uint256) {
+        return address(this).balance;
     }
 
 }
