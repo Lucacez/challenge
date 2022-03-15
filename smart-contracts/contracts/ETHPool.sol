1399fc060c28acc05eb5520e8700ecb528b506c9
C-Mierez <carlos.mierez20@gmail.com>
2022-03-15 00:05:25 -0300

    Major: Added reward system

--- smart-contracts/contracts/ETHPool.sol
+++ smart-contracts/contracts/ETHPool.sol
@@ -23,6 +23,9 @@ contract ETHPool is Ownable {
     /// @notice The total amount of rewards yet to be distributed
     uint256 private _totalRewardBalance;
 
+    /// @notice Counts the total amount of times admins have deposited rewards.
+    uint256 private _totalRewardCount;
+
     /// @notice CURRENT total cumulative rewards per token
     uint256 public totalRewardPerTokenMask;
 
@@ -51,7 +54,11 @@ contract ETHPool is Ownable {
     /* -------------------------------------------------------------------------- */
     event StakedETH(address indexed user, uint256 amount);
     event WithdrawnETH(address indexed user, uint256 amount);
-    event DepositedRewards(uint256 blockNumber, uint256 amount);
+    event DepositedRewards(
+        uint256 indexed depositNumber,
+        uint256 blockNumber,
+        uint256 amount
+    );
     event ClaimedRewards(address indexed user, uint256 amount);
 
     /* -------------------------------------------------------------------------- */
@@ -63,11 +70,18 @@ contract ETHPool is Ownable {
         _;
     }
 
-    modifier updateUserRewards(address account) {
+    /// @notice Update the user's pending rewards and shift its mask accordingly.
+    modifier updateUserRewards(address _account) {
         // Update the user's pending rewards
-        userRewards[account].pendingAmount = getUserPendingRewards(account);
+        userRewards[_account].pendingAmount = getUserPendingRewards(_account);
         // Update the user's reward mask
-        userRewardPerTokenMask[account] = totalRewardPerTokenMask;
+        userRewardPerTokenMask[_account] = totalRewardPerTokenMask;
+        _;
+    }
+
+    /// @notice Update the contract's total rewards mask with the newest rewards.
+    modifier updateTotalRewards(uint256 _reward) {
+        totalRewardPerTokenMask = getNextTotalRewardMask(_reward);
         _;
     }
 
@@ -86,8 +100,8 @@ contract ETHPool is Ownable {
         emit StakedETH(msg.sender, msg.value);
     }
 
-    /// @notice Withdraws the user's staked tokens and updates the pool's balance.
-    function withdrawETH() public updateUserRewards(msg.sender) {
+    /// @notice Withdraws all the user's staked tokens and updates the pool's balance.
+    function withdrawAllETH() public updateUserRewards(msg.sender) {
         uint256 staked = userStakedBalance[msg.sender];
         require(staked > 0, "Must have staked ETH to withdraw.");
 
@@ -101,6 +115,27 @@ contract ETHPool is Ownable {
         emit WithdrawnETH(msg.sender, staked);
     }
 
+    /// @notice Withdraws an exact amount of user's staked tokens and updates the pool's balance.
+    /// @param _amount the exact amount of tokens to withdraw.
+    function withdrawExactETH(uint256 _amount)
+        public
+        updateUserRewards(msg.sender)
+    {
+        uint256 staked = userStakedBalance[msg.sender];
+        require(staked > 0, "Must have staked ETH to withdraw.");
+        require(_amount > 0, "Must unstake a positive value.");
+        require(staked >= _amount, "Not enough ETH staked.");
+
+        userStakedBalance[msg.sender] = staked - _amount;
+        _totalPoolBalance -= _amount;
+
+        // AFAIK This is the preferred way of transferring ETH currently
+        (bool sent, ) = msg.sender.call{value: _amount}("");
+        require(sent, "Failed to send Ether.");
+
+        emit WithdrawnETH(msg.sender, _amount);
+    }
+
     /* --------------------------------- Views --------------------------------- */
     /// @return the current total of staked tokens.
     function getPoolBalance() public view returns (uint256) {
@@ -112,21 +147,72 @@ contract ETHPool is Ownable {
         return _totalRewardBalance;
     }
 
-    function getUserPendingRewards(address account)
+    /// @notice Subtract the user's mask from the total mask to obtain the rewards
+    /// per token, and multiply that by the user's staked balance.
+    /// Accumulate the result with the already pending rewards.
+    /// @param _account the user's address.
+    /// @return the user's pending rewards.
+    function getUserPendingRewards(address _account)
         public
         view
         returns (uint256)
     {
         return
-            (userStakedBalance[account] *
-                (totalRewardPerTokenMask - userRewardPerTokenMask[account])) +
-            userRewards[account].pendingAmount;
+            ((userStakedBalance[_account] *
+                (totalRewardPerTokenMask - userRewardPerTokenMask[_account])) /
+                1e18) + userRewards[_account].pendingAmount;
+    }
+
+    /// @notice Calculate the next total rewards mask, accumulating the pool's
+    /// current balance and a specific reward.
+    /// @param _reward the amount of reward to be distributed in the current pool.
+    /// @return the next total rewards mask.
+    function getNextTotalRewardMask(uint256 _reward)
+        public
+        view
+        returns (uint256)
+    {
+        if (_totalPoolBalance == 0) {
+            return 0;
+        }
+        return totalRewardPerTokenMask + ((_reward * 1e18) / _totalPoolBalance);
     }
 
     /* --------------------------------- Rewards -------------------------------- */
     /// @notice Admin's function to distribute rewards to stakers.
-    function depositRewards() public payable onlyOwner positiveEthValue {
+    function depositRewards()
+        public
+        payable
+        onlyOwner
+        positiveEthValue
+        updateTotalRewards(msg.value)
+    {
         _totalRewardBalance += msg.value;
-        emit DepositedRewards(block.number, msg.value);
+        _totalRewardCount += 1;
+        emit DepositedRewards(_totalRewardCount, block.number, msg.value);
+    }
+
+    /// @notice Claim all the user's pending rewards.
+    function claimRewards() public updateUserRewards(msg.sender) {
+        UserReward storage uReward = userRewards[msg.sender];
+        uint256 pending = uReward.pendingAmount;
+        require(uReward.pendingAmount > 0, "User has no pending rewards.");
+        require(
+            address(this).balance >= pending,
+            "CRITICAL: Contract has no ETH."
+        );
+        require(
+            _totalRewardBalance >= pending,
+            "CRITICAL: Contract has no rewards left to distribute."
+        );
+
+        uReward.pendingAmount = 0;
+        uReward.totalAmount += pending;
+        _totalRewardBalance -= pending;
+
+        (bool sent, ) = msg.sender.call{value: pending}("");
+        require(sent, "Failed to send Ether.");
+
+        emit ClaimedRewards(msg.sender, pending);
     }
 }
